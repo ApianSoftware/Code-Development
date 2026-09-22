7cef980aaba887c36c8e58634d45443553f6636f
Apian Holdings <cjhankins958@gmail.com>
2026-09-22 12:49:09 -0400

    Harden examples/go/bounded_worker.go bounded-work example

--- examples/go/bounded_worker.go
+++ examples/go/bounded_worker.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"sync"
+
 	"golang.org/x/sync/errgroup"
 )
 
+const maxJobs = 10_000
+
 func run(ctx context.Context, jobs []string) error {
+	if len(jobs) > maxJobs {
+		return fmt.Errorf("job batch exceeds max size: %d", maxJobs)
+	}
+
 	g, ctx := errgroup.WithContext(ctx)
 	g.SetLimit(32)
 
-	var mu sync.Mutex
 	results := make([]string, 0, len(jobs))
+	var mu sync.Mutex
 
 	for _, job := range jobs {
 		job := job
